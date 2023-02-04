5deeb8848f5b0e8924c84a5f5e13076f45aa0834
old_urchin <drtibibo@yandrx.ru>
2023-02-04 18:12:16 +0300

    Добавлено:Вводная часть, функция NumberFromString - позваляет пользователю вводить число в консоли

---
+++ Endtest1/Program.cs
@@ -0,0 +1,36 @@
+﻿const int A = 3;
+Console.WriteLine("Программа сортирует массив строк и выделяет элементы массива, ");
+Console.WriteLine($"длина которых меньше {A} символов в отдельный массив.");
+Console.WriteLine("Генерируем массив для сортировки:");
+Console.WriteLine("Определим длину этого массива.");
+
+NumberFromString(out double number);
+
+void NumberFromString(out double number)
+{
+    string? text;   
+    
+    while (true)
+    {
+        Console.WriteLine("Введите целое положительное число, больше 0:");
+        text = Console.ReadLine();
+        if (double.TryParse(text, out number))
+        {
+            if ((number > 0) & (number == Convert.ToInt32(number)))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Это число не является подходящим.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Не удалось распознать число.");
+        }
+
+    }
+      
+}
+
