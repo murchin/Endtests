e64544615b1ae0e775e71b32e30acdd4bddedd73
old_urchin <drtibibo@yandrx.ru>
2023-02-04 18:15:50 +0300

    Добавлено:Функция GenerationRandomStringArray -создает string массив, заполненный случайным образом сгенерированными элементами

--- Endtest1/Program.cs
+++ Endtest1/Program.cs
@@ -5,6 +5,10 @@ Console.WriteLine("Генерируем массив для сортировки
 Console.WriteLine("Определим длину этого массива.");
 
 NumberFromString(out double number);
+int LengthRandomArray = Convert.ToInt32(number);
+
+GenerationRandomStringArray(LengthRandomArray, out string[]RandomStringArray);
+
 
 void NumberFromString(out double number)
 {
@@ -32,5 +36,29 @@ void NumberFromString(out double number)
 
     }
       
+}
+
+void GenerationRandomStringArray(int LengthRandomArray, out string[]RandomStringArray)
+{
+    string[] SimbolsArray ={"1","2","3","4","5","6","7","8","9","0","-","=","!","@","#","$","%","^","&","*","(",
+")","_","+","`","~","q","w","e","r","t","y","u","i","o","p","[","]","a","s","d","f","g","h","j","k","l","z","x",
+"x","c","v","b","n","m","/","Q","W","E","R","T","Y","U","I","O","P","{","}","|","A","S","D","F","G","H","J","K",
+"L",":","Z","X","C","V","B","N","M","<",">","?"};
+RandomStringArray = new string[LengthRandomArray];
+    const int MaxLengthElement = 5;
+    string ElementArray = string.Empty;
+    int LengthElementArray;
+    Random rand = new Random();
+
+    for (int index = 0; index < LengthRandomArray; index++)
+    {
+        LengthElementArray = rand.Next(MaxLengthElement);
+        for (int index1 = 0; index1 <= LengthElementArray; index1++)
+        {
+            ElementArray = ElementArray + SimbolsArray[rand.Next(0, SimbolsArray.Length - 1)];
+        }
+        RandomStringArray[index] = ElementArray;
+        ElementArray = string.Empty;
+    }
 }
 
